ba90845ea716d8db8cd3242528838f8dfd9eaf3b
David Čáp <capdavidd@gmail.com>
2020-01-28 19:52:29 +0100

    removed test Box, cleanup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Calendar from './components/Calendar';
-import Box from './components/Box';
 import './App.css';
 
 function App() {
-    const [boxState, setBoxState] = useState(0);
-    const moveBox = () => {
-        const updatedState = boxState + 1;
-        setBoxState(updatedState);
-    };
-
     return (
         <div className="App">
             <Calendar />
-            <button onClick={moveBox}>MOVE</button>
-            <Box move={boxState}>HI</Box>
         </div>
     );
 }

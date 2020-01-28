447fda2189938b61069fb4c63348b2b7c8efe9ec
David Čáp <capdavidd@gmail.com>
2020-01-28 20:08:50 +0100

    adding comment with the assignemt

--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ import calendarStoreConfig from './hooks/calendarStore';
 
 calendarStoreConfig();
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// https://paper.dropbox.com/doc/Interview-Calendar-AbEOjCiQQyBhKY8o6W2Pi

98fce3e650feb049e83a0ec6f3ee6c5076638c9c
David Čáp <capdavidd@gmail.com>
2020-01-28 19:57:05 +0100

    styles cleanup, deleted comments, added eslint exception for dependancy arr

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,45 +1,41 @@
-import React, { useEffect, useCallback, Fragment } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import styled from '@emotion/styled';
 
 import { useStore } from '../hooks/store';
 import TimesTable from '../containers/TimesTable';
-import DaysTable from '../containers/DaysTable';
+import DaysWrapper from '../containers/DaysWrapper';
 import Spinner from '../components/Spinner';
+
+const StyledDiv = styled.div`
+    margin: 0 auto;
+    max-width: 615px;
+    display: flex;
+    box-shadow: rgba(0, 0, 0, 0.1) 0 0 17px;
+    border-radius: 10px;
+    overflow: hidden;
+`;
+
+const CalendarContent = () => (
+    <Fragment>
+        <TimesTable />
+        <DaysWrapper />
+    </Fragment>
+);
 
 const Calendar = props => {
     const [state, dispatch] = useStore();
-    const setCalendar = useCallback(
-        data => {
-            dispatch('SET_CALENDAR', data);
-        },
-        [dispatch]
-    );
+
     useEffect(() => {
         fetch('https://interview-calendar-backend.herokuapp.com/api/calendar')
             .then(response => response.json())
             .then(responseData => {
-                setCalendar(responseData.calendar);
+                dispatch('SET_CALENDAR', responseData.calendar);
             })
             .catch(err => console.log(err));
-    }, [setCalendar]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    const calendar = (
-        <Fragment>
-            <TimesTable />
-            <DaysTable />
-        </Fragment>
-    );
-
-    const StyledDiv = styled.div`
-        margin: 0 auto;
-        max-width: 615px;
-        /* background: blue; */
-        display: flex;
-        box-shadow: rgba(0, 0, 0, 0.1) 0 0 17px;
-        border-radius: 10px;
-        overflow: hidden;
-    `;
-    return <StyledDiv>{state.loading ? <Spinner /> : calendar}</StyledDiv>;
+    return <StyledDiv>{state.loading ? <Spinner /> : <CalendarContent />}</StyledDiv>;
 };
 
 export default Calendar;

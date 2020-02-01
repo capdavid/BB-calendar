75d8f933eb220be83d4b2e7f341b53710adadc3a
David Čáp <capdavidd@gmail.com>
2020-02-01 12:31:48 +0100

    renamed state to store since it's a global store not a local one

--- src/containers/DaysWrapper.js
+++ src/containers/DaysWrapper.js
@@ -1,48 +1,48 @@
-import React, { useState, useCallback } from 'react';
-import styled from '@emotion/styled';
-import { useStore } from '../hooks/store';
+import React, { useState, useCallback } from "react";
+import styled from "@emotion/styled";
+import { useStore } from "../hooks/store";
 
-import DaysTable from '../containers/DaysTable';
-import ScrollButton from '../components/ScrollButton';
+import DaysTable from "../containers/DaysTable";
+import ScrollButton from "../components/ScrollButton";
 
 const StyledDaysWrapper = styled.div`
-    width: 70%;
-    display: flex;
-    position: relative;
+  width: 70%;
+  display: flex;
+  position: relative;
 `;
 
 const DaysWrapper = () => {
-    const state = useStore(false)[0];
-    const [scrollState, setScrollState] = useState(0);
+  const store = useStore()[0];
+  const [scrollState, setScrollState] = useState(0);
 
-    const scrollHandler = useCallback(
-        direction => {
-            let scrollPos = scrollState;
-            let maxDayCount;
-            if (window.innerWidth < 800) {
-                maxDayCount = state.calendar.length - 2;
-            } else {
-                maxDayCount = state.calendar.length - 3;
-            }
+  const scrollHandler = useCallback(
+    direction => {
+      let scrollPos = scrollState;
+      let maxDayCount;
+      if (window.innerWidth < 800) {
+        maxDayCount = store.calendar.length - 2;
+      } else {
+        maxDayCount = store.calendar.length - 3;
+      }
 
-            if (direction === 'left' && scrollPos > 0) {
-                scrollPos--;
-            } else if (direction !== 'left' && scrollPos < maxDayCount) {
-                scrollPos++;
-            }
-            setScrollState(scrollPos);
-        },
-        [scrollState, state.calendar.length]
-    );
-    return (
-        <StyledDaysWrapper>
-            <ScrollButton left onClick={() => scrollHandler('left')}>
-                &lt;
-            </ScrollButton>
-            <ScrollButton onClick={scrollHandler}>></ScrollButton>
-            <DaysTable scrollPosition={scrollState} />
-        </StyledDaysWrapper>
-    );
+      if (direction === "left" && scrollPos > 0) {
+        scrollPos--;
+      } else if (direction !== "left" && scrollPos < maxDayCount) {
+        scrollPos++;
+      }
+      setScrollState(scrollPos);
+    },
+    [scrollState, store.calendar.length]
+  );
+  return (
+    <StyledDaysWrapper>
+      <ScrollButton left onClick={() => scrollHandler("left")}>
+        &lt;
+      </ScrollButton>
+      <ScrollButton onClick={scrollHandler}>></ScrollButton>
+      <DaysTable scrollPosition={scrollState} />
+    </StyledDaysWrapper>
+  );
 };
 
 export default DaysWrapper;
--- src/containers/TimesTable.js
+++ src/containers/TimesTable.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
 
-import { useStore } from '../hooks/store';
+import { useStore } from "../hooks/store";
 
-import TableCell from '../components/TableCell';
+import TableCell from "../components/TableCell";
 
 export const TimesTable = () => {
-    const state = useStore()[0];
+  const store = useStore()[0];
 
-    const StyledTimeTable = styled.div`
-        display: flex;
-        flex-direction: column;
-        width: 30%;
-        text-align: center;
-        align-items: center;
-        justify-content: center;
-        box-shadow: rgba(0, 0, 0, 0.1) 0 2px 9px;
-        z-index: 1;
-    `;
+  const StyledTimeTable = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    box-shadow: rgba(0, 0, 0, 0.1) 0 2px 9px;
+    z-index: 1;
+  `;
 
-    return (
-        <StyledTimeTable>
-            <TableCell heading>Zvolte čas</TableCell>
-            {state.calendar[0]?.timeslots.map((time, index) => (
-                <TableCell
-                    key={time.id}
-                    selectedTime={index === state.selectedCell[1] ? true : false}
-                >{`${time.from} - ${time.to}`}</TableCell>
-            ))}
-        </StyledTimeTable>
-    );
+  return (
+    <StyledTimeTable>
+      <TableCell heading>Zvolte čas</TableCell>
+      {store.calendar[0]?.timeslots.map((time, index) => (
+        <TableCell
+          key={time.id}
+          selectedTime={index === store.selectedCell[1] ? true : false}
+        >{`${time.from} - ${time.to}`}</TableCell>
+      ))}
+    </StyledTimeTable>
+  );
 };
 
 export default TimesTable;

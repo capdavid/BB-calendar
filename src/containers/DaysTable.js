2e30be5a029c28f4a71da81734fc553b3e8d1690
David Čáp <capdavidd@gmail.com>
2020-02-01 12:28:28 +0100

    renamed state to store and exctracted logic from jsx

--- src/containers/DaysTable.js
+++ src/containers/DaysTable.js
@@ -1,45 +1,40 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
 
-import DayTable from '../components/DayTable';
-import { useStore } from '../hooks/store';
+import DayTable from "../components/DayTable";
+import { useStore } from "../hooks/store";
 
 const StyledDaysTable = styled.div`
-    width: 100%;
-    display: flex;
-    position: relative;
-    transform: ${props => `translateX(calc(-${props.scrollPosition}*33%))`};
-    transition: all 0.5s;
-    @media (max-width: 800px) {
-        transform: ${props => `translateX(calc(-${props.scrollPosition}*50%))`};
-    }
+  width: 100%;
+  display: flex;
+  position: relative;
+  transform: ${props => `translateX(calc(-${props.scrollPosition}*33.34%))`};
+  transition: all 0.5s;
+  @media (max-width: 800px) {
+    transform: ${props => `translateX(calc(-${props.scrollPosition}*50%))`};
+  }
 `;
 
 const DaysTable = props => {
-    const state = useStore()[0];
+  const store = useStore()[0];
 
-    const scrollBasedOnProps = () => {
-        let scrollPosition;
-        scrollPosition = props.scrollPosition;
-        return scrollPosition;
-    };
-
-    const scrollPosition = scrollBasedOnProps();
-
-    return (
-        <StyledDaysTable scrollPosition={scrollPosition}>
-            {state.calendar.map((day, index) => (
-                <DayTable
-                    dayIndex={index}
-                    //can I not send a prop? or I have to send it with null?
-                    selectedCell={state.selectedCell[0] === index ? state.selectedCell[1] : null}
-                    key={day.date}
-                    times={day.timeslots}
-                    date={day.date}
-                />
-            ))}
-        </StyledDaysTable>
-    );
+  return (
+    <StyledDaysTable {...props}>
+      {store.calendar.map(({ date, timeslots }, index) => {
+        const selectedIndex =
+          store.selectedCell[0] === index ? store.selectedCell[1] : null;
+        return (
+          <DayTable
+            dayIndex={index}
+            selectedCell={selectedIndex}
+            key={date}
+            times={timeslots}
+            date={date}
+          />
+        );
+      })}
+    </StyledDaysTable>
+  );
 };
 
 export default DaysTable;

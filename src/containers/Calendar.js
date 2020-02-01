4b1e76c2d09a39c11e6689f594fd0ba90d78cc05
David Čáp <capdavidd@gmail.com>
2020-02-01 12:30:43 +0100

    moved Calendar to cointainers

---
+++ src/containers/Calendar.js
@@ -0,0 +1,46 @@
+import React, { useEffect, Fragment } from "react";
+import styled from "@emotion/styled";
+
+import { useStore } from "../hooks/store";
+import TimesTable from "../containers/TimesTable";
+import DaysWrapper from "../containers/DaysWrapper";
+import Spinner from "../components/Spinner";
+
+const StyledDiv = styled.div`
+  margin: 0 auto;
+  max-width: 615px;
+  display: flex;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 0 17px;
+  border-radius: 10px;
+  overflow: hidden;
+  @media (max-width: 800px) {
+    max-width: 400px;
+  }
+`;
+
+const CalendarContent = () => (
+  <Fragment>
+    <TimesTable />
+    <DaysWrapper />
+  </Fragment>
+);
+
+const Calendar = props => {
+  const [store, dispatch] = useStore();
+
+  useEffect(() => {
+    fetch("https://interview-calendar-backend.herokuapp.com/api/calendar")
+      .then(response => response.json())
+      .then(responseData => {
+        dispatch("SET_CALENDAR", responseData.calendar);
+      })
+      .catch(err => console.log(err));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <StyledDiv>{store.loading ? <Spinner /> : <CalendarContent />}</StyledDiv>
+  );
+};
+
+export default Calendar;

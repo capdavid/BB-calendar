ae6a749650f7b777f3154223b9ea71cb3ca8d23b
David Čáp <capdavidd@gmail.com>
2020-02-07 22:31:52 +0100

    cleanup

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -1,16 +1,16 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 const ScrollButton = styled.button`
-    padding: 0.2rem 0.3rem;
-    background: #fff;
-    border: 1px solid #bbb;
-    position: absolute;
-    top: 1.15rem;
-    right: ${props => (props.left ? '' : '0')};
-    font-weight: bold;
-    outline: none;
-    z-index: 2;
-    cursor: pointer;
+  padding: 0.2rem 0.3rem;
+  background: #fff;
+  border: 1px solid #bbb;
+  position: absolute;
+  top: 1.15rem;
+  right: ${props => (props.left ? "" : "0")};
+  font-weight: bold;
+  outline: none;
+  z-index: 2;
+  cursor: pointer;
 `;
 
 export default ScrollButton;

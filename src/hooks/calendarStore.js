fc2b2578d9aaed1d27d53d850e7335fb20dfed71
David Čáp <capdavidd@gmail.com>
2020-02-08 22:48:38 +0100

    changed selectedCell store property to an object for better readability

--- src/hooks/calendarStore.js
+++ src/hooks/calendarStore.js
@@ -5,22 +5,22 @@ const storeConfig = () => {
     FETCH_CALENDAR_REQUEST: () => {
       return { loading: true };
     },
-    FETCH_CALENDAR_SUCCESS: (curState, updatedState) => {
+    FETCH_CALENDAR_SUCCESS: (_curState, updatedState) => {
       return { calendar: updatedState, loading: false };
     },
-    FETCH_CALENDAR_FAIL: (curState, err) => {
+    FETCH_CALENDAR_FAIL: (_curState, err) => {
       console.log(err);
       return { error: true };
     },
-    SELECT_TIMESLOT: (curState, dayIndex, cellIndex) => {
-      return { selectedCell: [dayIndex, cellIndex] };
+    SELECT_TIMESLOT: (_curState, dayIndex, cellIndex) => {
+      return { selectedCell: { dayIndex, cellIndex } };
     }
   };
   initStore(actions, {
     calendar: [],
     loading: false,
     error: false,
-    selectedCell: []
+    selectedCell: {}
   });
 };
 
